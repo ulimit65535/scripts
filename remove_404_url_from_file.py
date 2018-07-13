8880fc2c2d39fdad897b5579960528fd879bcf62
ulimit65535 <2510823550@qq.com>
2018-07-13 11:00:53 +0800

    aa

---
+++ remove_404_url_from_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import requests
+import sys
+from urllib.parse import urlparse
+
+IP="10.2.2.202"
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    f = open(filename)
+    for line in f.readlines():
+        line = line.strip()
+        urlp =urlparse(line)
+        try:
+            response = requests.get("http://" + IP + urlp.path, headers={ "Host": urlp.netloc })
+        except:
+            pass
+
+        if response.status_code == 200:
+            print(line)
+
+    f.close()
